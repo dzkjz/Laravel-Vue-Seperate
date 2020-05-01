df213a2987716d5456c1326d2d475e2ace7c2a55
dzkjz <dzkjzdzkjz@gmail.com>
2020-05-01 21:40:12 +0800

    在 Vue 组件中使用 Vuex 模块

--- resources/assets/js/pages/Home.vue
+++ resources/assets/js/pages/Home.vue
@@ -1,10 +1,34 @@
 <template>
+    <div>
+        <span v-show="cafesLoadStatus===1">加载中...</span>
+        <span v-show="cafesLoadStatus===2">数据加载成功！</span>
+        <span v-show="cafesLoadStatus===3">数据加载失败！</span>
 
+        <ul>
+            <li v-for="cafe in cafes">{{cafe.name}}</li>
+        </ul>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "Home.vue"
+        name: "Home.vue",
+        created() {
+            this.$store.dispatch('loadCafes');
+        },
+        /**
+         * 定义组件的计算属性
+         */
+        computed: {
+            // 获取 cafes 加载状态
+            cafesLoadStatus() {
+                return this.$store.getters.getCafesLoadStatus;
+            },
+            // 获取 cafes
+            cafes() {
+                return this.$store.getters.getCafes;
+            }
+        }
     }
 </script>
 
